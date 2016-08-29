07836aaf5bff5a59b7678cdf8598d4290598a8bd
HamadaGabrIbrahim <Hamada.G.Ibrahim@gmail.com>
2016-08-29 04:42:44 -0500

    Implementing property DAO

--- src/main/java/property/org/mum/realEstate/DAO/PropertyDAO.java
+++ src/main/java/property/org/mum/realEstate/DAO/PropertyDAO.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 import property.org.mum.realEstate.domain.Category;
 import property.org.mum.realEstate.domain.Property;
+import property.org.mum.realEstate.domain.SavedProperty;
 
 public interface PropertyDAO {
 	List<Property> getAllProperies();
 
 	List<Property> getProperitiesByCategory(Category category);
 
+	Property getPropertyById(int id);
+
 	void addNewProperty(Property property);
 
 	void deleteCategory(Category category);
 
-	void addPropertyToSaved(Property property);
+	void addPropertyToSaved(SavedProperty sProperty);
 }
--- src/main/java/property/org/mum/realEstate/DAOImpl/PropertyDAOImpl.java
+++ src/main/java/property/org/mum/realEstate/DAOImpl/PropertyDAOImpl.java
@@ -1,44 +1,97 @@
 package property.org.mum.realEstate.DAOImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Repository;
 
 import property.org.mum.realEstate.DAO.PropertyDAO;
 import property.org.mum.realEstate.domain.Category;
 import property.org.mum.realEstate.domain.Property;
+import property.org.mum.realEstate.domain.SavedProperty;
 
 @Repository
-public class PropertyDAOImpl implements PropertyDAO{
+public class PropertyDAOImpl implements PropertyDAO {
+	private SessionFactory sessionFactory;
+
+	// private static final Configuration configuration = new
+	// Configuration().configure();
+	public PropertyDAOImpl() {
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+	}
 
 	@Override
 	public List<Property> getAllProperies() {
+		List<Property> properties = new ArrayList<Property>();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Property");
+		properties = query.getResultList();
+		session.getTransaction().commit();
+		session.close();
 		// TODO Auto-generated method stub
-		return null;
+		return properties;
 	}
 
 	@Override
 	public List<Property> getProperitiesByCategory(Category category) {
+		List<Property> properties = new ArrayList<Property>();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Property where category=:category");
+		query.setParameter("category", category);
+		properties = query.getResultList();
+		session.getTransaction().commit();
+		session.close();
 		// TODO Auto-generated method stub
-		return null;
+		return properties;
 	}
 
 	@Override
 	public void addNewProperty(Property property) {
-		// TODO Auto-generated method stub
-		
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(property);
+		session.getTransaction().commit();
+		session.close();
+
 	}
 
 	@Override
 	public void deleteCategory(Category category) {
-		// TODO Auto-generated method stub
-		
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.delete(category);
+		session.getTransaction().commit();
+		session.close();
+
 	}
 
 	@Override
-	public void addPropertyToSaved(Property property) {
-		// TODO Auto-generated method stub
-		
+	public void addPropertyToSaved(SavedProperty aProperty) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(aProperty);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	@Override
+	public Property getPropertyById(int id) {
+		Property property = new Property();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from Property where id=:id");
+		query.setParameter("id", id);
+		property = (Property) query.getSingleResult();
+		session.getTransaction().commit();
+		session.close();
+		return property;
 	}
 
 }
