7ae16b06f31826ab690c3b795470ab2e1867caa9
ekru <ekram.gebremedhin@gmail.com>
2016-09-01 20:07:25 -0500

    Final property updates

--- src/main/java/property/org/mum/realEstate/DAO/AddressDAO.java
+++ src/main/java/property/org/mum/realEstate/DAO/AddressDAO.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 import property.org.mum.realEstate.domain.Address;
-import property.org.mum.realEstate.domain.Category;
+
 @Transactional
 public interface AddressDAO extends JpaRepository<Address, Integer>{
 
